f26f8d9fb726c677ccfe8ef3af3a9be2561bc845
KhachatryanHakob <hakob.kh.2003@gmail.com>
2025-07-10 12:05:33 +0400

    Update bash.sh
    asdfsa

--- bash.sh
+++ bash.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
+!/bin/bash
+set -e
+
 source info.env
 
 if grep -q "my-app" "$1" ; then
 
-docker build -t dockerf1:latest .
+  docker build -t dockerf1:latest .
 
-docker tag dockerf1:latest myusername/dockerf1:latest
+  docker tag dockerf1:latest khachatryanhakob/dockerf1:latest
 
-docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+  echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 
-docker push khachatryanhakob/dockerf1:latest
+  docker push khachatryanhakob/dockerf1:latest
 
-docker run -it -p 8081:8081 --name my-container khachatryanhakob/dockerf1:latest 
+  docker run -d -p 8081:8081 --name my-container khachatryanhakob/dockerf1:latest
 
 else
-   echo "there is not my-app"
+  echo "there is not my-app"
 fi
